d6d6303cb10db5ce3caad966a27e22669e4a11e2
Your Name <e@w.com>
2022-09-04 16:25:58 +0300

    Сортировка элементов строк массива по убыванию

---
+++ Task_54/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 54: В двумерном массиве упорядочить по убыванию элементы каждой строки.
+
+Console.WriteLine("Введите количество строк:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] TwoArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(50);
+        }
+    }
+    return matrix;
+}
+
+void EnterArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+
+    }
+}
+void Change(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            if (matrix[i, j] < matrix[i, j + 1])
+            {
+
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, j + 1];
+                matrix[i, j + 1] = temp;
+
+            }
+
+
+        }
+        for (int k = 0; k < matrix.GetLength(0); k++)
+
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+
+                }
+
+
+            }
+
+        }
+
+    }
+}
+
+
+
+int[,] resultMatrix = TwoArray(rows, columns);
+EnterArray(resultMatrix);
+Console.WriteLine();
+Change(resultMatrix);
+EnterArray(resultMatrix);
